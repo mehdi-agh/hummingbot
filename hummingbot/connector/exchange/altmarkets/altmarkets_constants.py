110cd21a289fe86b766d3afc61b54c380561024b
Mehdi Aghazadeh <65454478+mehdi-agh@users.noreply.github.com>
2023-02-11 02:19:10 +0300

    Update altmarkets_constants.py

--- hummingbot/connector/exchange/altmarkets/altmarkets_constants.py
+++ hummingbot/connector/exchange/altmarkets/altmarkets_constants.py
@@ -3,7 +3,7 @@ from hummingbot.core.api_throttler.data_types import RateLimit, LinkedLimitWeigh
 
 # A single source of truth for constant variables related to the exchange
 class Constants:
-    EXCHANGE_NAME = "altmarkets"
+    EXCHANGE_NAME = "mSamex"
     REST_URL = "https://msamex.com/api/v2/peatio"
     WS_PRIVATE_URL = "wss://msamex.com/api/v2/ranger/private"
     WS_PUBLIC_URL = "wss://msamex.com/api/v2/ranger/public"
